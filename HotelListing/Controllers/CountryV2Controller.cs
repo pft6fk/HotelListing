d1c28edf605c33d9736fb52cfc746c12f9e1551a
Mukhammadaziz Juraev <juraev.mukhammadaziz@gmail.com>
2022-02-10 21:59:51 +0500

    implemented version control to an API

---
+++ HotelListing/Controllers/CountryV2Controller.cs
@@ -0,0 +1,26 @@
+﻿using HotelListing.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HotelListing.Controllers
+{
+    [ApiVersion("2.0")]
+    [Route("api/country")]
+    [ApiController]
+    public class CountryV2Controller : ControllerBase
+    {
+        private DatabaseContext _context;
+        public CountryV2Controller(DatabaseContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> GetCountries()
+        {
+            return Ok(_context.Countries) ;
+        }
+    }
+}
--- HotelListing/ServiceExtensions.cs
+++ HotelListing/ServiceExtensions.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
@@ -73,6 +74,16 @@ namespace HotelListing
                         }.ToString());
                     }
                 });
+            });
+        }
+
+        public static void ConfigureVersioning(this IServiceCollection services)
+        {
+            services.AddApiVersioning(opt =>
+            {
+                opt.ReportApiVersions = true;
+                opt.AssumeDefaultVersionWhenUnspecified = true;
+                opt.DefaultApiVersion = new ApiVersion(1, 0);
             });
         }
     }
